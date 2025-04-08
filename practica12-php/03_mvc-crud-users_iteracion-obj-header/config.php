444ad930d0047d8fffdbe62c719b4ab77c187c85
gegebe <ggirbes@gmail.com>
2025-04-08 20:22:15 +0200

    problems closing session

--- practica12-php/03_mvc-crud-users_iteracion-obj-header/config.php
+++ practica12-php/03_mvc-crud-users_iteracion-obj-header/config.php
@@ -5,8 +5,8 @@
     // define('RUTA_WEB', 'http://localhost:8888/curso_php/practica12-php/03_mvc-crud-users_iteracion-obj-header/index.php');
 
     define('HOST', 'localhost'); 
-    define('USER', 'developer');
-    define('PASS', '1234');
+    define('USER', 'root');
+    define('PASS', 'root');
     define('DB', 'appweb');
     define('PORT', '3306');
 
